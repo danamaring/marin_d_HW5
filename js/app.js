5d2c3becce3a62301c02d55548663108e9c1fb1e
Marin Garcia <d_maringarcia@tss-hr420-dm22.fanshawec.ca>
2019-07-16 13:19:25 -0400

    hamburguer menu

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 (() => {
-	console.log("fired!");
+	console.log('fired!');
+
+	let mobileNav = document.querySelector('.nav'),
+		navPanel = document.querySelector('#burgerCon');
+
+	function toggleNav() {
+		console.log('should show nav dropdown');
+		navPanel.classList.toggle('show-mobile-nav');
+	}
+
+
+
+	mobileNav.addEventListener('click', toggleNav);
+
+
 
 })();
